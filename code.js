9bfc81d69c6f26be7acd0c781bfa566316b5b0cb
Julius Tarng <julius@webflow.com>
2020-05-14 14:36:06 -0500

    Init with basic scale down

---
+++ code.js
@@ -0,0 +1,8 @@
+// Scales selection down by 2
+const sel = [...figma.currentPage.selection];
+sel.forEach(n => {
+    n.resize(n.width / 2, n.height / 2);
+});
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
